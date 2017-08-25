084ea94debd5ad839764e7919b2147225d96ecbf
Andrius Semionovas <aneworld@gmail.com>
2017-08-25 14:47:42 +0300

    Init

---
+++ src/main/java/Bar.java
@@ -0,0 +1,6 @@
+/**
+ * @author Andrius Semionovas
+ * @since 2017-08-25
+ */
+public class Bar {
+}
--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,6 +1,15 @@
+import com.google.auto.value.AutoValue;
+
 /**
  * @author Andrius Semionovas
  * @since 2017-08-25
  */
+
+@AutoValue
 public class Foo {
+    public int id = 42;
+
+    public void foo(Bar bar) {
+        System.out.print(id);
+    }
 }
